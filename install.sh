85f2fb81dc74cd3113d3b0f0738fc2ff519468e9
Biswajit Dhar <34776555+bdhar1@users.noreply.github.com>
2022-09-02 15:16:46 +0000

    Installation script scratchpad

---
+++ install.sh
@@ -0,0 +1,45 @@
+
+az aks get-credentials -n aks-dapr-demo -g rg-dapr-demo
+
+# Register features
+az feature register --namespace "Microsoft.ContainerService" --name "AKS-ExtensionManager"
+az feature register --namespace "Microsoft.ContainerService" --name "AKS-Dapr"
+
+# For checking
+az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/AKS-ExtensionManager')].{Name:name,State:properties.state}"
+az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/AKS-Dapr')].{Name:name,State:properties.state}"
+
+# Registering
+az provider register --namespace Microsoft.KubernetesConfiguration
+az provider register --namespace Microsoft.ContainerService
+
+# Add extension to CLI
+az extension add --name k8s-extension
+
+az k8s-extension create --cluster-type managedClusters \
+--cluster-name aks-dapr-demo \
+--resource-group rg-dapr-demo \
+--name myDaprExtension \
+--extension-type Microsoft.Dapr \
+--auto-upgrade-minor-version true
+
+# Add KEDA AKS features to CLI
+az extension add --upgrade --name aks-preview
+
+# KEDA features
+az feature register --namespace Microsoft.ContainerService --name AKS-KedaPreview
+
+# for checking
+az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/AKS-KedaPreview')].{Name:name,State:properties.state}"
+
+# Refreshing registration
+az provider register --namespace Microsoft.ContainerService
+
+# Enable in AKS Cluster
+az aks update \
+  --resource-group rg-dapr-demo \
+  --name aks-dapr-demo \
+  --enable-keda
+
+# Check for KEDA enabled state
+az aks show -g "rg-dapr-demo" -n "aks-dapr-demo" --query "workloadAutoScalerProfile.keda.enabled" 
